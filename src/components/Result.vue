3bd8ead3a5c0a377755fd97c0cb82f7e1ffd2480
gnoul_ <ttluong4869@gmail.com>
2022-03-08 14:57:18 +0700

    Implement result component

--- src/components/Result.vue
+++ src/components/Result.vue
@@ -1,11 +1,34 @@
 <template>
     <p>Reaction time: {{ score }} ms</p>
+    <p class="rank">{{ rank }}</p>
 </template>
 
 <script>
 export default {
     props: ['score'],
+    data() {
+        return {
+            rank: null,
+        };
+    },
+    mounted() {
+        if (this.score < 100) {
+            this.rank = 'Inhuman reaction 👀';
+        } else if (this.score < 200) {
+            this.rank = "That's fast 😲";
+        } else if (this.score < 400) {
+            this.rank = 'Nice reaction 😉';
+        } else {
+            this.rank = 'Is that your fastest reaction? 😳';
+        }
+    },
 };
 </script>
 
-<style></style>
+<style>
+.rank {
+    font-size: 1.4rem;
+    color: #0faf87;
+    font-weight: bold;
+}
+</style>
