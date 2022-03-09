fba17b8c3b8577d8f82ab15afc5b93155172bf5f
gnoul_ <ttluong4869@gmail.com>
2022-03-09 10:49:27 +0700

    Update footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,5 +1,8 @@
 <template>
-    <div class="footer">Created by <strong>gnoul_</strong></div>
+    <div class="footer">
+        Created by
+        <a href="https://github.com/gnoul4869" target="_blank"><strong>gnoul_</strong> </a>
+    </div>
 </template>
 
 <script>
@@ -7,6 +10,14 @@ export default {};
 </script>
 
 <style>
+a {
+    color: inherit;
+    text-decoration: inherit;
+}
+a:hover {
+    transition: all 0.35s;
+    color: #0faf87;
+}
 .footer {
     position: fixed;
     bottom: 1.25rem;
